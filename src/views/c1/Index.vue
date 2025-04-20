b1ded79d72b396edc4d0b13ceecfb8691e7bac0f
FanofHOTS <144080370+FanofHOTS@users.noreply.github.com>
2025-04-20 13:58:14 +0700

    Update Index.vue

--- src/views/c1/Index.vue
+++ src/views/c1/Index.vue
@@ -125,8 +125,11 @@
       </div>
     </div>
     <div class="text-center md:text-left">
+      <router-link class="align-baseline font-bold text-sm text-gray-500 hover:text-gray-700 mr-5" :to="{ name: 'truec1' }">
+        &larr; Chương trước
+      </router-link>
       <router-link class="align-baseline font-bold text-sm text-gray-500 hover:text-gray-700 mr-5" :to="{ name: 'home' }">
-        &larr; Về trang chủ
+        Trang chủ
       </router-link>
       <router-link class="align-baseline font-bold text-sm text-blue-500 hover:text-blue-700" :to="{ name: 'c2' }">
         Chương kế &rarr;
